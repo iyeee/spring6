cebf3897bb84cd61e639998f89d4013e9ba768af
iyeee <1009699416@qq.com>
2022-12-09 17:58:24 +0800

    4.spring对IOC的实现

---
+++ spring6-003-dependency-injection/src/main/java/com/powernode/spring6/bean/Cat.java
@@ -0,0 +1,7 @@
+package com.powernode.spring6.bean;/**
+ * @Author iyeee
+ * @Date 2022/12/9 0:59
+ *  @Version 1.0.1
+ */
+public class Cat {
+}
---
+++ spring6-003-dependency-injection/src/main/java/com/powernode/spring6/jdbc/MyDataSource2.java
@@ -0,0 +1,7 @@
+package com.powernode.spring6.jdbc;/**
+ * @Author iyeee
+ * @Date 2022/12/9 14:56
+ *  @Version 1.0.1
+ */
+public class MyDataSource2 {
+}
